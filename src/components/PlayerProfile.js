d98ab9d299724be39af70f00a15a2fb44e378217
nichro02 <nichro02@gmail.com>
2021-02-15 11:08:46 -0700

    Added delete user route

--- src/components/PlayerProfile.js
+++ src/components/PlayerProfile.js
@@ -1,44 +1,106 @@
 import { useState, useEffect } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
 
-import { profile } from '../services/profile.service'
-import { getCurrentUser } from '../services/auth.service'
+import { profile, deleteUser } from '../services/profile.service'
+import { getCurrentUser, logout } from '../services/auth.service'
+import {removeItem} from '../utilities/localStorage.utilities'
 
-import { useParams } from 'react-router-dom'
+import { resMessage } from '../utilities/message.utilities'
 
+
+
+import { Box, Button, Container } from '@chakra-ui/react'
 
 const PlayerProfile = () => {
+    //set state for error message
+    const [message, setMessage] = useState("")
     const [playerProfile, setPlayerProfile] = useState([])
+
+    let history = useHistory()
 
     const currentUser = getCurrentUser()
     const userId = currentUser.data.id
-    //console.log(currentUser)
     const { id } = useParams()
-    console.log(id)
 
-    // const getPlayer = () => {
-    //     return profile(userId).then(user => {
-    //         const info = user.data
-    //         console.log(info)
-    //         setPlayerProfile(info)
-    //     })
-            
-    // } 
+    
+    
+    useEffect(() => {
+        profile(id).then((response)=> {
+            console.log(response.data.data)
+            setPlayerProfile(response.data.data)
+        }).catch(error => {
+            console.log(error)
+        })
+    }, [])
+    
 
-    profile(id).then(user => {
-        const info = user.data.data
-        console.log(info)
-        return(
-            <div>
-                {info.player.username}
-            </div>
-        )
-        //.then(info => {setPlayerProfile(info)})
-    })
+    // const getPlayerScores = profile(id).then((response)=> {
+    //     const data = response.data.data
+    //     setPlayerProfile(data)
+    // }).catch(error => {
+    //     console.log(error)
+    // })
 
+    console.log(playerProfile)
+
+    // const playerScores = playerProfile.games
+
+    // const showScores = playerScores.reverse().map((score, index) => {
+    //     return(
+    //     <Box key={score.id}>{score.score}</Box>
+    //     )
+    // })
+    
+    
+    // profile(id).then(user => {
+    //     const info = user.data.data
+    //     console.log(info)
+    //     return(
+    //         <div>
+    //             {info.player.username}
+    //         </div>
+    //     )
+    //     //.then(info => {setPlayerProfile(info)})
+    // })
+
+    const deleteButton = () => {
+        console.log(userId)
+        console.log(id)
+        if(currentUser.data.id == id){
+            return <Button onClick={handleDelete}>Delete My Profile</Button>
+        }
+    }
+
+    const handleDelete = () => {
+        logout()
+        deleteUser(id).then((response) => {
+            console.log(response)
+            if(response.data.status.code === 200){
+                //send user to homepage with successful login
+                history.push('/home')
+                window.location.reload()
+            }
+            else {
+                setMessage(resMessage(response.data.status))
+            }
+        })
+    }
+    
     return(
-        <div>
-            PLAYER PROFILE
-        </div>
+        <Box>
+            <Box>
+               Username
+            </Box>
+            <Box>
+                {deleteButton()}
+            </Box>
+            
+            <Box>
+                <strong>Recent Games</strong>
+                
+            </Box>
+            
+        </Box>
     )
 
 
